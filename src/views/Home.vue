b3709a81832a253b9d6545881271f0b898241361
adonismis <adonismis@gmail.com>
2022-02-25 17:00:29 +0800

    add fontBase

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    Home
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  // components: {
+  //   HelloWorld
+  // },
+  mounted () {
+    console.log(this.$http.get)
+    alert('test')
   }
 }
 </script>
